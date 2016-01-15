250bd6500cb7cba885ea18f98486c53fda111140
Faris Ansari <netchamp.faris@gmail.com>
2016-01-15 13:49:33 +0530

    improved button ui

---
+++ app/scripts/services/restapiurl.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name bvcoeDmsApp.firebaseurl
+ * @description
+ * # firebaseurl
+ * Constant in the bvcoeDmsApp.
+ */
+angular.module('bvcoeDmsApp')
+  .constant('firebaseurl', 'https://hazri.firebaseio.com');
